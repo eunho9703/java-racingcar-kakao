be50d1b603ae2969c44eefb9fa87c1a84c430376
eunho9703 <eunho9703@gmail.com>
2024-04-01 16:16:37 +0900

    refactor: 유효성 검증 시점 변경

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -8,22 +8,25 @@ import static java.util.Arrays.*;
 import static java.util.stream.Collectors.toList;
 
 public class CarFactory {
-    private static Set<String> NAME_SET = new HashSet<>();
+
     public static final int START_SCORE = 1;
 
     public static List<Car> createCar(String[] carNameList) {
+        validateDuplicateName(asList(carNameList));
+
         return stream(carNameList)
                 .map(carName -> {
-                    validateDuplicateName(carName);
                     return new Car(carName, START_SCORE);
                 })
                 .collect(toList());
     }
 
-    private static void validateDuplicateName(String name) {
-        if (NAME_SET.contains(name)) {
+    private static void validateDuplicateName(List<String> nameList) {
+        Set<String> nameSet = Set.copyOf(nameList);
+
+        if (nameList.size() != nameSet.size()) {
             throw new IllegalArgumentException("동일한 차량 이름은 불가능합니다.");
         }
-        NAME_SET.add(name);
+
     }
 }
